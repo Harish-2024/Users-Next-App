4e1f1e3c2fe56a9d68eb20bfb987d37bd79234cf
harish <harish2024sharma@gmail.com>
2025-02-17 10:17:51 +0530

    Added the project to the git

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="bg-black p-5 text-white">
+          <NavBar />
+        </div>
         {children}
       </body>
     </html>
